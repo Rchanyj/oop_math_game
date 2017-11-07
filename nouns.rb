a9e2028d93d7c3ae4099a61722e50055933ca249
Raionos <rchan1992@gmail.com>
2017-11-07 05:36:10 +0000

    added nouns planner

---
+++ nouns.rb
@@ -0,0 +1,14 @@
+class Player
+  #Each player can enter responses via command lines
+  #Each player has 3 lives
+end
+
+class Turns
+  #Controls start and end of game
+  #Controls turns in each round; generates math questions
+  #Sees if entered answers are correct (gets.chomp)
+    #stdin/stdout
+  #Picks 2 random numbers between 1-20 to use
+  #Each turn ends upon a player entering a response (right or wrong)
+  #Players alternate per turn
+end
