a70ffd8cccd5ee8c4639cc5dfd4e9581f70588a2
Raionos <rchan1992@gmail.com>
2017-11-07 21:25:34 +0000

    changed player names

--- player1.rb
+++ player1.rb
@@ -1,7 +1,7 @@
 require './game_module'
 
 player1 = MathGame::Player.new
-player1.name = "Ron"
+player1.name = "player1"
 
 $player1_name = player1.name
 
--- player2.rb
+++ player2.rb
@@ -1,6 +1,6 @@
 require './game_module'
 
 player2 = MathGame::Player.new
-player2.name = "Harry"
+player2.name = "player2"
 
 $player2_name = player2.name
