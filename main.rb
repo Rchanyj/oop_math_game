ea4e09d233ebce4f90f68325157aed24f6782c7d
Raionos <rchan1992@gmail.com>
2017-11-08 01:15:01 +0000

    added function for users to enter player names

--- main.rb
+++ main.rb
@@ -3,8 +3,16 @@ require './player1'
 require './player2'
 
 #runs game
+
 new_game = MathGame::Turn.new
-new_game.start_game($player1_name, $player2_name)
+#new_game.start_game($player1_name, $player2_name)
+puts "Player 1, enter your name!"
+$player1 = gets.chomp
+puts "Player 2, enter your name!"
+$player2 = gets.chomp
+
+new_game.start_game($player1, $player2)
+
 
 
 
